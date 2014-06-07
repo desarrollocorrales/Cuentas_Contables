299210e0d7c2742543e96507ec2aa96d2c42512b
desarrollocorrales <desarrollo@loscorrales.com.mx>
2014-06-07 10:12:49 -0500

    Cambios en la DAL

--- ImportarCuentasContables/clases/FirebirdDAL.cs
+++ ImportarCuentasContables/clases/FirebirdDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using FirebirdSql.Data.FirebirdClient;
 using System.Data;
+using System.Text.RegularExpressions;
 
 namespace ImportarCuentasContables.clases
 {
@@ -71,6 +72,7 @@ namespace ImportarCuentasContables.clases
                                     VALUES
                                        ( -1, {0}, {1}, '{2}' )", 
                                        idMicroCuentaPadre ,oCliente.iConsecutivo, nombre);
+                Comando.CommandText = Regex.Replace(Comando.CommandText, @"[\r\n\x00\x1a\\'""]", @"\$0");
                 Comando.ExecuteNonQuery();
 
                 if (Conexion.State != System.Data.ConnectionState.Closed)
@@ -84,106 +86,5 @@ namespace ImportarCuentasContables.clases
                 throw ex;
             }
         }
-
-        /*public List<Cliente> getClientesSinCuentaCo()
-        {
-            Conexion.ConnectionString = getConnectionString();
-            try
-            {
-                Conexion.Open();
-                Comando = new FbCommand(string.Empty, Conexion);
-                Comando.CommandText =
-                    string.Format(@"  SELECT 
-                                          CLIENTE_ID, NOMBRE, CUENTA_CXC 
-                                        FROM 
-                                          CLIENTES 
-                                       WHERE 
-                                          CUENTA_CXC IS NULL 
-                                    ORDER BY 
-                                          CLIENTE_ID");
-
-                DataTable tbResultado = new DataTable();
-                Adapter.SelectCommand = Comando;
-                Adapter.Fill(tbResultado);
-
-                Cliente oCliente;
-                List<Cliente> lstClientes = new List<Cliente>();
-                foreach (DataRow row in tbResultado.Rows)
-                {
-                    oCliente = new Cliente();
-                    oCliente.ID = Convert.ToInt32(row["CLIENTE_ID"]);
-                    oCliente.Nombre = Convert.ToString(row["NOMBRE"]);
-                    oCliente.Cuenta_CO = Convert.ToString(row["CUENTA_CXC"]);
-                    lstClientes.Add(oCliente);
-                }
-
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-
-                return lstClientes;
-            }
-            catch (Exception ex)
-            {
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-
-                throw ex;
-            }
-        }*/
-
-        /*public void ActualizarCliente(Cliente cliente)
-        {
-            Conexion.ConnectionString = getConnectionString();
-            try
-            {
-                Conexion.Open();
-                Comando = new FbCommand(string.Empty, Conexion);
-                Comando.CommandText =
-                    string.Format(@"UPDATE CLIENTES 
-                                       SET CUENTA_CXC = '{0}' 
-                                     WHERE CLIENTE_ID = {1}",
-                                     cliente.Cuenta_CO, 
-                                     cliente.ID);
-                Comando.ExecuteNonQuery();
-
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-
-                throw ex;
-            }
-        }*/
-
-        /*public void InsertarCuentaContable(Cliente cliente)
-        {
-            Conexion.ConnectionString = getConnectionString();
-            try
-            {
-                Conexion.Open();
-                Comando = new FbCommand(string.Empty, Conexion);
-                Comando.CommandText =
-                    string.Format(@"INSERT INTO CUENTAS_CO 
-                                       (CUENTA_ID, CUENTA_PADRE_ID, SUBCUENTA)
-                                    VALUES
-                                       (-1, {0},{1})",
-                                     cliente.ID,
-                                     cliente.iConsecutivo);
-                Comando.ExecuteNonQuery();
-
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                if (Conexion.State != System.Data.ConnectionState.Closed)
-                    Conexion.Close();
-
-                throw ex;
-            }
-        }*/
     }
 }
